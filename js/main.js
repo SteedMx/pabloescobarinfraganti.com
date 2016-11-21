2a89c21d6861f59d333f309920de8d38f52f6393
matgianesini <matias.rodgian@gmail.com>
2016-11-21 12:29:04 -0600

    Change images, set scrollmagic proper values

--- js/main.js
+++ js/main.js
@@ -1,19 +1,6 @@
 /* Using jQuery */
 (function($) {
 
-  var strength1 = 50;
-  var strength2 = 100;
-  var strength3 = 200;
-  $("html").mousemove(function(e){
-      var pageX = e.pageX - ($(window).width() / 2);
-      var pageY = e.pageY - ($(window).height() / 2);
-      var newvalueX = 1* pageX * -1;
-      var newvalueY = 1* pageY * -1;
-      $('#background').css("background-position", (strength1 / $(window).width() * pageX * -1)+"px "+(strength1  / $(window).height() * pageY * -1)+"px");
-      $('#middleground').css("background-position", (strength2 / $(window).width() * pageX * -1)+"px "+(strength2  / $(window).height() * pageY * -1)+"px");
-      $('#foreground').css("background-position", (strength3 / $(window).width() * pageX * -1)+"px "+(strength3  / $(window).height() * pageY * -1)+"px");
-  });
-
   var wh = window.innerHeight,
     $iphone = $('.iphone'),
     $innerS1 = $('.innerS1'),
@@ -61,7 +48,7 @@
   // iPhone intro animation Timeline
   var iphoneIntroTl = new TimelineMax();
   iphoneIntroTl
-    .from($iphone, 1, {yPercent: 20,xPercent: 100, ease: Power4.easeInOut})
+    .from($iphone, 1, {yPercent: -20,xPercent: 100, ease: Power4.easeInOut})
     .to($innerS1, 0.5, {opacity: 0, yPercent: -5, scale: 0.98}, '0');
 
   // Hook iphone animation onto the scrollbar
